b52a03c349ca6883f1341bf264aae17c3c234fc4
Alberto Silva <asilvadesigns@gmail.com>
2017-03-16 09:40:55 -0400

    getting aquainted with stores

--- app/index.js
+++ app/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReacDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Root from 'Views/Root';
 
+const reducer = (state, action) => {
+  if (action.type === 'INC') {
+    return state+action.payload;
+  }
+  return state;
+  console.log("state:", state);
+  console.log("action:", action);
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log('store changed', store.getState())
+})
+
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+
 ReacDOM.render(
-  <Root/>,
+  <Provider store={store}>
+    <Root/>
+  </Provider>,
   document.getElementById('app')
 );
